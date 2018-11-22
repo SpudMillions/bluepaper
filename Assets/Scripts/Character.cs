a729a327d723aae704f1fc3f852ff48eb2ca7e47
spudmillions <joshuaadams18@gmail.com>
2018-11-21 19:52:05 -0500

    initial commit with player movement

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public abstract class Character : MonoBehaviour {
+
+	[SerializeField] private float _speed;
+	protected Vector2 Direction;
+
+	private Animator _animator;
+
+	private void Start ()
+	{
+		_animator = GetComponent<Animator>();
+	}
+	
+	protected virtual void Update ()
+	{
+		Move();
+	}
+	
+	public void Move()
+	{
+		transform.Translate(Direction * _speed * Time.deltaTime);
+		
+		if (Direction.x != 0 || Direction.y != 0) //player is moving
+		{
+			AnimateMovement(Direction);
+		}
+		else
+		{
+			_animator.SetLayerWeight(1,0);
+		}
+		
+		
+	}
+
+	public void AnimateMovement(Vector2 direction)
+	{
+		_animator.SetLayerWeight(1,1);
+		
+		//set animation params so player faces correct direction
+		_animator.SetFloat("horizontal", direction.x);
+		_animator.SetFloat("vertical", direction.y);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+	protected override void Update ()
+	{
+		GetInput();
+		base.Update();
+	}
+
+	private void GetInput()
+	{
+		Direction = Vector2.zero;
+		
+		if (Input.GetKey(KeyCode.W))
+		{
+			Direction += Vector2.up;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			Direction += Vector2.left;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			Direction += Vector2.down;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			Direction += Vector2.right;
+		}
+	}
+}
